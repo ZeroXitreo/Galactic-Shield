332542f70ed291ffd8cf9307d1cda402b98dd5d8
ZeroXitreo <3112566+ZeroXitreo@users.noreply.github.com>
2024-02-04 22:26:41 +0100

    Update sh_shield.lua

--- lua/components/sh_shield.lua
+++ lua/components/sh_shield.lua
@@ -31,7 +31,7 @@ if CLIENT then
             ScrH() - sizeY - paddingH + (sizeY + paddingH) * (1 - component.chipMovement),
             sizeX,
             sizeY,
-            component:AllowedToPickup(self.owner, self.entity) != false and galactic.theme.colors.green or galactic.theme.colors.red)
+            component:AllowedToPickup(LocalPlayer(), self.entity) != false and galactic.theme.colors.green or galactic.theme.colors.red)
         draw.DrawText(
             ownerName,
             font,
@@ -43,7 +43,19 @@ if CLIENT then
 end
 
 if SERVER then
-    function component:PlayerSpawnedEffect(ply, model, ent)
+
+    local PLAYER = FindMetaTable("Player")
+    if PLAYER.AddCount then
+        galactic.PLAYERAddCount = galactic.PLAYERAddCount or PLAYER.AddCount
+        function PLAYER:AddCount(Type, ent) -- self: Player
+            if IsValid(self) and IsValid(ent) then
+                component:OnEntitySpawn(self, ent)
+            end
+            return galactic.PLAYERAddCount(self, Type, ent)
+        end
+    end
+
+    function component:PlayerSpawnedEffect(ply, _, ent)
         component:OnEntitySpawn(ply, ent)
     end
     
@@ -51,19 +63,24 @@ if SERVER then
         component:OnEntitySpawn(ply, ent)
     end
     
-    function component:PlayerSpawnedProp(ply, model, ent)
+    function component:PlayerSpawnedProp(ply, _, ent)
         component:OnEntitySpawn(ply, ent)
     end
     
-    function component:PlayerSpawnedRagdoll(ply, model, ent)
+    function component:PlayerSpawnedRagdoll(ply, _, ent)
         component:OnEntitySpawn(ply, ent)
     end
     
     function component:PlayerSpawnedSENT(ply, ent)
+        print(ent)
         component:OnEntitySpawn(ply, ent)
     end
     
     function component:PlayerSpawnedSWEP(ply, ent)
+        component:OnEntitySpawn(ply, ent)
+    end
+    
+    function component:PlayerSpawnedVehicle(ply, ent)
         component:OnEntitySpawn(ply, ent)
     end
     
@@ -82,14 +99,28 @@ function component:PhysgunPickup(ply, ent)
     end
 end
 
+function component:CanTool(ply, tbl)
+    local ent = tbl.Entity
+    if not IsValid(ent) then return end
+    if self:CoveredUnderPP(ent) then
+        return self:AllowedToTool(ply, ent)
+    end
+end
+
 function component:CoveredUnderPP(ent)
     return not ent:IsWorld() and not ent:IsPlayer() and ent:GetNWEntity("shield_owner")
 end
 
 function component:AllowedToPickup(ply, ent)
+    if not ent:GetNWEntity("shield_owner"):IsPlayer() then return end
     -- if ply:IsSuperAdmin() then return end
-    if ply != ent:GetNWEntity("shield_owner") then return false end
-    return
+    if ply:SteamID64() != ent:GetNWEntity("shield_owner"):SteamID64() then return false end
+end
+
+function component:AllowedToTool(ply, ent)
+    if not ent:GetNWEntity("shield_owner"):IsPlayer() then return end
+    -- if ply:IsSuperAdmin() then return end
+    if ply:SteamID64() != ent:GetNWEntity("shield_owner"):SteamID64() then return false end
 end
 
 galactic:Register(component)
